595f6236f9ec3945fab58b429d8241d85d3b33c8
Abhishek Dharmadhikari <abhishekdharmadhikari25@gmail.com>
2021-11-03 00:53:00 +0530

    Testing_Using_PostmanContd

--- http-server.py
+++ http-server.py
@@ -1,23 +1,42 @@
 from socket import *
 import sys
+from datetime import date
+from methods import *
+
 host = '127.0.0.1' # address for our server
-port = 1200 # port for our server
+port = 12001 # port for our server
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-serverSocket.bind(host, port)
+serverSocket.bind((host, port))
 serverSocket.listen(2)
+
+methods = ['GET','HEAD','POST','PUT','DELETE']
 
 while True:
     connectionSocket, address = serverSocket.accept()
     print("Connected by", address)
     message_request = connectionSocket.recv(1024)
     #parse request to request class and sepearte request line , headers , message body from request message received from client
-    client_request  = request(message_request)
+    client_request  = request(message_request.decode())
     #below fucntion will internally call get_request_line,get_headers,get_message_body function
     client_request.parse_request()
+    #if request not in gven method then there must be malformed syntax in request method so send 400 bad error as response
+    if (client_request.request_method not in methods):
+        response = 'HTTP/1.1 501 NOT implemented\r\n'
+        response += 'Server : HTTP-server\r\n'
+        today = date.today()
+        today = today.strftime("%d/%m/%Y")
+        response += 'Date :'+today+'\r\n'
+        response += '\r\n'
+        connectionSocket.send(response.encode())
+        continue
+    #if request in given methods but we will be implementing so far 5 methods only then 
     if (client_request.request_method == 'GET'):
-    elif client_request.request_method == 'HEAD'):
+        connectionSocket.send("Hello".encode())
+    elif (client_request.request_method == 'HEAD'):
+        continue
     elif (client_request.request_method == 'DELETE'):
+        continue
     #connectionSocket.sendall(data)
     #connectionSocket.close()
--- methods.py
+++ methods.py
@@ -1,4 +1,3 @@
-#Will contain class requests and response corresponding to each header.
 '''
 All Request headers to be implemented:
                       | Accept                   ; Section 14.1
@@ -11,22 +10,50 @@ All Request headers to be implemented:
                       | If-Unmodified-Since      ; Section 14.28
                       | Range                    ; Section 14.35
                       | User-Agent               ; Section 14.43
+                       |response-header = Accept-Ranges           ; Section 14.5
+                       | ETag                    ; Section 14.19
+                       | Location                ; Section 14.30
+                      # | Retry-After             ; Section 14.37
+                       | Server                  ; Section 14.38
+                    |entity-header  = Allow      ; Section 14.7
+                      | Content-Encoding         ; Section 14.11
+                      | Content-Language         ; Section 14.12
+                      | Content-Length           ; Section 14.13
+                      | Content-Location         ; Section 14.14
+                      | Content-MD5              ; Section 14.15
+                      | Content-Range            ; Section 14.16
+                      | Content-Type             ; Section 14.17
+                      | Expires                  ; Section 14.21
+                      | Last-Modified            ; Section 14.29
+                      General headers:
+                             general-header = 
+                      | Connection               ; Section 14.10
+                      | Date                     ; Section 14.18(TO be done)
+                      | Trailer                  ; Section 14.40
+                      | Transfer-Encoding        ; Section 14.41
+
+The
 '''
 class request:
     #constructor 
     def __init__(self,http_request):
         self.http_request = http_request
+        return
 
     #request_line= method space Request-URL space HTTP-version CRLF
     def get_request_line(self):
-        self.request_line = http_request.split('\r\n')[0]
-        self.request_method = self.request_line.split(' ')[0]
+        try:
+            self.request_line = self.http_request.split('\r\n')[0]
+            self.request_method = self.request_line.split(' ')[0]
         #Request-URI    = "*" | absoluteURI | abs_path | authority
         #absolute URI for proxy server(we are not implementing proxy server 
         # * for option method , authority for connect method .those will not be implemented
-        self.request_URI = self.request_line.split(' ')[1](#abs path)
-        self.http_version = self.request_line.split(' ')[2]
-        self.request_line = self.request_line + '\r\n'       
+            self.request_URI = self.request_line.split(' ')[1] #abs path
+            self.http_version = self.request_line.split(' ')[2]
+            self.request_line = self.request_line + '\r\n'
+        except:
+            print("Error in Syntax:")
+        return
 
     #3 header types: General Headers, Request header , Entity headers
     #message-header = field-name ":" [ field-value ]
@@ -35,7 +62,7 @@ class request:
         temp = self.http_request.split('\r\n')
         j=0
         for i in range(len(temp)):
-            if temp[i]=='':
+            if temp[i] == '' :
                 j=i
                 break
         for i in range(1,j):
@@ -48,7 +75,7 @@ class request:
         j=0
         temp = self.http_request.split('\r\n')
         for i in range(len(temp)):
-            if temp[i]==''
+            if temp[i]=='':
                 j=i
                 break
         self.message_body = temp[j+1:]
@@ -60,14 +87,50 @@ class request:
     def parse_request(self):
         self.get_request_line()
         self.extract_request_headers()
+        self.print_http_request()
+        print("Printing now headers:")
+        print(self.request_headers)
+        print("MEthod :",self.request_method,"version:",self.http_version,"Request URI:",self.request_URI,"Requestline:",self.request_line+"fjjdf\n")
+        return
 
-class get_response(request):
-    def response_line(self):
-    def status_code(self):
-    def construct_response(self):
-    def send_response(self):
-        #call all other functions
+    def accept(self):
+        return
+    def accept_encoding(self):
+        return
+    def host(self):
+        return
+    def accept_charset(self):
+        return
+    def user_agent(self):
+        return
+    def range(self):
+        return
 
-class head_response(get_response):
+def error_in_get_request(request):
+    if(len(request.request_line.split(' '))!=3):
+        return 1
+    else if(len(request
+    
+def construct_get_response(request):
+    #if(error_in_get_request(request)):
+        #print("Error 400")
+    #3. If the host as determined by rule 1 or 2 is not a valid host on the server, the response MUST be a 400 (Bad Request) error message.
+    #if host header not present or host is n
+    #elif(request_URI_has_proxy(request))#like get absolute uri 
+``  #elif(request.http_version!='HTTP/1.1'):
+        #print("Error 505:HTTP VErsion Not supported")
+    #if(request.request_URI.find('?')!=-1)::#if request_URI too long then throw error
+        #wheIn post is converted forcefully to GEt then throw this error
+    #else:
+    #Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
 
-class delete_response(request):
+    status_code = "XXX"
+    status_line = 'HTTP/1.1' + status_code + response_phrase + '\r\n'
+    #Date: Tue, 15 Nov 1994 08:12:31 GMT(Date format:)
+    message_body = " "
+    response_message = status_line + response + message_body
+    return response_message
+def construct_head_response(request):
+    return
+def construct_delete_response(request):
+    return
